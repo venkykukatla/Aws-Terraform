c21cd4b7507123c48c8f57f3984675e29cda02a7
subbu <subbuitengineer@gmail.com>
2023-06-02 19:54:32 +0530

    modify

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     encrypt = true    
-#     bucket = "hella-bucketsrytg"
-#     dynamodb_table = "terraform-state-lock-dynamo"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "hella-bucketsrytg"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
