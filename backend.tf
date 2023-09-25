e20818cbeb11133eebd31fbede0df4a6c422f2f4
venkykukatla <57248099+venkykukatla@users.noreply.github.com>
2023-09-25 22:12:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt = true    
-    bucket = "hella-bucketsryt"
+    bucket = "mybbhufgyyhhf"
     dynamodb_table = "terraform-state-lock-dynamo"
     key    = "terraform.tfstate"
     region = "us-east-1"
